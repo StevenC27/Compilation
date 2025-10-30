ddc7717085ee7b503568e4a8317291d2d3a6c985
Steven Curran <stevencurran.sc@gmail.com>
2025-10-30 23:10:41 +0000

    moved analysing to the LexHelper class

--- package_starter/Lex.java
+++ package_starter/Lex.java
@@ -1,133 +1,20 @@
-import java.util.*;
-
 public class Lex {
 	String stream;
+	LexHelper lexHelper;
 
 	Lex(String input) {
 		stream = input;
+		lexHelper = new LexHelper(stream);
 	}
 	
 	LexToken[] getTokens() {
-		/*
-			STRING LITERAL:
-				- check for " and put into string mode.
-				- append each character to the token.
-				- check for closing " to end string mode and end the token.
-			GENERAL:
-				- if there is white space then end the token
-				- if the token is empty then add the current char into the token.
-
-			IDENTIFIER:
-				- if there is syntax then end the token.
-
-			+SYNTAX:
-				- if the syntax is anything but = then it ends after the single character.
-				- if the syntax is = then the next character is checked and must be another = to create == token.
-				- if the syntax is == then the next character is checked and if it is another = then it is an unknown token.
-
-			+KEYWORD:
-				- if the current token is a keyword but the next token is another alphabetic or digit character then the token is not a keyword
-					for example "ifg" is an identifier.
-
-			NUMERIC LITERALS:
-				- must be an uninterrupted sequence of numeric characters from 0...9.
-
-			+TYPE NAMES:
-				- if the current token is a keyword but the next token is another alphabetic or digit character then the token is not a keyword
-					for example "intq" is an identifier.
-
-			---- PLAN ----
-			STEP 1: check the type of the current token.
-			STEP 2: check that the token should end.
-			STEP 3: create a new token of the correct type and add it to the list.
-		*/
-
-		ArrayList<LexToken> tokenArrayList = new ArrayList<>();
-		String currentToken = "";
-		int lineNumber = 1;
-		int characterOffset = 0;
-		boolean stringMode = false;
-
-		boolean tokenEnd = false;
-		boolean returnChar = false;
-		boolean lineChar = false;
-		boolean tabChar = false;
-		boolean spaceChar = false;
-
-		int tokenType = LexToken.UNKNOWN;
-
-		for(int i = 0; i < stream.length(); i++) {
-			char currentChar = stream.charAt(i);
-
-			if (currentChar == '"' && !stringMode){
-				stringMode = true;
-				tokenType = LexToken.LITERAL_STRING;
-				tokenEnd = true;
-			} else if (stringMode){
-				if (currentChar == '"'){
-					stringMode = false;
-					tokenEnd = true;
-				}
-			} else {
-				//STEP 1: check the type of the current token.
-				tokenType = LexHelper.checkToken(currentToken);
-
-				//STEP 2: check if the token should end.
-				tokenEnd = LexHelper.isTokenEnd(currentToken, currentChar, tokenType);
-
-				if (currentChar == '\r'){
-					returnChar = true;
-				} else if (currentChar == '\n'){
-					lineChar = true;
-				} else if (currentChar == '\t'){
-					tabChar = true;
-				} else if (currentChar == ' '){
-					spaceChar = true;
-				}
-			}
-
-			if (tokenEnd){
-				if (currentToken.isEmpty()){
-					tokenEnd = false;
-				} else {
-					tokenArrayList.add(new LexToken(tokenType, lineNumber, characterOffset, currentToken));
-					characterOffset += currentToken.length();
-					tokenEnd = false;
-					currentToken = "";
-				}
-
-				if (tokenType == LexToken.LITERAL_STRING && !stringMode){
-					characterOffset += 2;
-				}
-
-				if (returnChar){
-					lineNumber++;
-					returnChar = false;
-				} else if (lineChar){
-					characterOffset = 0;
-					lineChar = false;
-				} else if (tabChar){
-					characterOffset += 4;
-					tabChar = false;
-				} else if (spaceChar){
-					characterOffset++;
-					spaceChar = false;
-				}
-
-
-				if (!(currentChar == '\r' || currentChar == '\n' || currentChar == '\t' || currentChar == ' ' || currentChar == '"')){
-					currentToken+=currentChar;
-				}
-			} else {
-				currentToken += currentChar;
-			}
-		}
-		LexToken[] tokens = new LexToken[tokenArrayList.size()];
-		tokens = tokenArrayList.toArray(tokens);
-		return tokens;
+		return lexHelper.getLexTokens().toArray(new LexToken[0]);
 	}
 	
 	LexError[] getErrors() {
+
+
+
 		return null;
 	}
 }
--- package_starter/LexHelper.java
+++ package_starter/LexHelper.java
@@ -1,33 +1,123 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LexHelper {
     public static final String[] KEYWORDS = {"if", "else", "while", "return"};
     public static final String[] SYNTAX_TOKENS = {"=", "==", "*", "/", "+", "-", "(", ")", "[", "]", "{", "}", ";", ","};
     public static final String[] TYPE_NAMES = {"int", "float", "string", "bool"};
 
-    public static boolean isTokenEnd(String currentToken, char currentChar, int tokenType){
-        if (tokenType == LexToken.KEYWORD || tokenType == LexToken.TYPE_NAME){
-            if (!(Character.isAlphabetic(currentChar) || Character.isDigit(currentChar))){
-                return true;
-            }
-        } else if (tokenType == LexToken.SYNTAX_TOKEN){
-            if (!(currentToken.endsWith(Character.toString(currentChar)) && isOperation(currentChar))){
-                return true;
-            }
-        } else if (tokenType == LexToken.IDENTIFIER){
-            if (LexHelper.checkTokenArray(LexHelper.SYNTAX_TOKENS, Character.toString(currentChar))){
-                return true;
-            }
-        } else if (tokenType == LexToken.LITERAL_NUMBER){
-            if (!Character.isDigit(currentChar)){
-                return true;
-            }
-        } else if (tokenType == LexToken.UNKNOWN){
-            return true;
-        }
+    private final String input;
+    private List<LexToken> lexTokens;
+    private List<LexError> lexErrors;
 
-        return currentChar == '\r' || currentChar == '\n' || currentChar == '\t' || currentChar == ' ';
+    public LexHelper(String input){
+        this.input = input;
+        analyse();
     }
 
-    public static boolean isOperation(char character){
+    public void analyse(){
+        lexTokens = new ArrayList<>();
+        String currentToken = "";
+        int lineNumber = 1;
+        int characterOffset = 0;
+        boolean stringMode = false;
+
+        boolean tokenEnd = false;
+        boolean returnChar = false;
+        boolean lineChar = false;
+        boolean tabChar = false;
+        boolean spaceChar = false;
+
+        int tokenType = LexToken.UNKNOWN;
+
+        for(int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+
+            if (currentChar == '"' && !stringMode){
+                stringMode = true;
+                tokenType = LexToken.LITERAL_STRING;
+                tokenEnd = true;
+            } else if (stringMode){
+                if (currentChar == '"'){
+                    stringMode = false;
+                    tokenEnd = true;
+                }
+            } else {
+                tokenType = checkToken(currentToken);
+
+                if (tokenType == LexToken.KEYWORD || tokenType == LexToken.TYPE_NAME){
+                    if (!(Character.isAlphabetic(currentChar) || Character.isDigit(currentChar))){
+                        tokenEnd = true;
+                    }
+                } else if (tokenType == LexToken.SYNTAX_TOKEN){
+                    if (!(currentToken.endsWith(Character.toString(currentChar)) && isOperation(currentChar))){
+                        tokenEnd = true;
+                    }
+                } else if (tokenType == LexToken.IDENTIFIER){
+                    if (checkTokenArray(LexHelper.SYNTAX_TOKENS, Character.toString(currentChar))){
+                        tokenEnd = true;
+                    }
+                } else if (tokenType == LexToken.LITERAL_NUMBER){
+                    if (!Character.isDigit(currentChar)){
+                        tokenEnd = true;
+                    }
+                } else if (tokenType == LexToken.UNKNOWN){
+                    tokenEnd = true;
+                }
+
+                if (currentChar == '\r'){
+                    returnChar = true;
+                    tokenEnd = true;
+                } else if (currentChar == '\n'){
+                    lineChar = true;
+                    tokenEnd = true;
+                } else if (currentChar == '\t'){
+                    tabChar = true;
+                    tokenEnd = true;
+                } else if (currentChar == ' '){
+                    spaceChar = true;
+                    tokenEnd = true;
+                }
+            }
+
+            if (tokenEnd){
+                if (currentToken.isEmpty()){
+                    tokenEnd = false;
+                } else {
+                    lexTokens.add(new LexToken(tokenType, lineNumber, characterOffset, currentToken));
+                    characterOffset += currentToken.length();
+                    tokenEnd = false;
+                    currentToken = "";
+                }
+
+                if (tokenType == LexToken.LITERAL_STRING && !stringMode){
+                    characterOffset += 2;
+                }
+
+                if (returnChar){
+                    lineNumber++;
+                    returnChar = false;
+                } else if (lineChar){
+                    characterOffset = 0;
+                    lineChar = false;
+                } else if (tabChar){
+                    characterOffset += 4;
+                    tabChar = false;
+                } else if (spaceChar){
+                    characterOffset++;
+                    spaceChar = false;
+                }
+
+                if (!(currentChar == '\r' || currentChar == '\n' || currentChar == '\t' || currentChar == ' ' || currentChar == '"')){
+                    currentToken+=currentChar;
+                }
+            } else {
+                currentToken += currentChar;
+            }
+        }
+    }
+
+    public boolean isOperation(char character){
         return (character == '+' || character == '-' || character == '*' || character == '/' || character == '=');
     }
 
@@ -46,7 +136,7 @@ public class LexHelper {
         return isIdentifier;
     }
 
-    public static int checkToken(String token){
+    public int checkToken(String token){
         if(checkTokenArray(KEYWORDS, token)) return LexToken.KEYWORD;
         else if(checkTokenArray(SYNTAX_TOKENS, token)) return LexToken.SYNTAX_TOKEN;
         else if(checkTokenArray(TYPE_NAMES, token)) return LexToken.TYPE_NAME;
@@ -55,7 +145,7 @@ public class LexHelper {
         return LexToken.UNKNOWN;
     }
 
-    public static boolean checkTokenArray(String[] array, String token){
+    public boolean checkTokenArray(String[] array, String token){
         boolean isElement = false;
         for (String element : array){
             if (token.equals(element)) {
@@ -66,7 +156,7 @@ public class LexHelper {
         return isElement;
     }
 
-    public static boolean checkDigit(String token){
+    public boolean checkDigit(String token){
         boolean isDigit = true;
         if (token.isEmpty()) return false;
         for (char character : token.toCharArray()){
@@ -76,5 +166,15 @@ public class LexHelper {
             }
         }
         return isDigit;
+    }
+
+    public boolean isValidCharacter(char character){
+        if (Character.isAlphabetic(character)) return true;
+        else if (Character.isDigit(character)) return true;
+        else return false;
+    }
+
+    public List<LexToken> getLexTokens(){
+        return lexTokens;
     }
 }
